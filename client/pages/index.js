087854aea416ac7fc754a57a8b04c4ff3fe04063
Azamat-afzalov <52107423+Azamat-afzalov@users.noreply.github.com>
2020-09-05 17:44:39 +0500

    issues with https and http

---
+++ client/pages/index.js
@@ -0,0 +1,12 @@
+import buildClient from '../api/build-client';
+const LandingPage = ({ currentUser }) => {
+    console.log(currentUser);
+
+    return <h1>Landing Page</h1>;
+};
+LandingPage.getInitialProps = async (ctx) => {
+    const {data} = await buildClient(ctx).get('/api/users/currentuser');
+    return data;
+};
+
+export default LandingPage;
